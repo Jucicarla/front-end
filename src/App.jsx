9d06079b0dfab6c4f7572b03a3b956f0bf037ca8
Jucicarla Pires <jucipires@gmail.com>
2023-11-14 00:46:39 +0000

    extração do conteudo Card

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,10 @@ function App() {
  
   return (
     <>
-      <div className="card">
-        <h2>Rich Sanches</h2>
-        <div className="tags">
-          <div className="tag">Status: Vivo</div>
-          <div className="tag">Espécie: Humana</div>
-          <div className="tag">Origem: Terra C-137</div>
-        </div>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
-
-      </div>
+      
       <br/>
       <Card/>
-      
+      <Card/>       
     </>
   )
 }
