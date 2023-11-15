6d28a9d32fdd630e52a969849dc9f505993212f9
Jucicarla Pires <jucipires@gmail.com>
2023-11-15 00:02:14 +0000

    Implementação da API para carregamento de dados

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,48 @@
+import { useEffect, useState } from 'react';
 import './App.css'
-import Card from './components/Card/Card'
+import Card from './components/Card/Card';
 
 function App() {
-  const item1={
+  const item1 = {
     name: "Rick Sanchez",
-    imageUrl:"https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-    tags: ["Status: Vivo", "Especie: humano", "Origem: Terrra C-137"]
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    tags: ["Status: Vivo", "Espécie: Humana", "Origem: Terra C-137"]
   }
-  const item2={
+
+  const item2 = {
     name: "Morty Smith",
-    imageUrl:"https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
     tags: ["Origem: Terra C-137"]
   }
-  const item3={
+
+  const item3 = {
     name: "Summer Smith",
-    imageUrl:"https://rickandmortyapi.com/api/character/avatar/3.jpeg"
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
   }
-  const itens = [item1,item2,item3]
- 
+
+  // const items = [item1, item2, item3]
+
+  // useState
+  const [items, setItems] = useState([])
+
+  async function carregarDadosApi() {
+    const apiUrl = "https://rickandmortyapi.com/api/character/"
+
+    const response = await fetch(apiUrl)
+
+    const body = await response.json()
+
+    setItems(body.results)
+  }
+
+  useEffect(function () {
+    carregarDadosApi()
+  }, [])
+
   return (
     <>
-      {itens.map(function(element){
-        return <Card item={element}/>
-      })}
       <div className="cards">
-        {itens.map(function (element) {
+        {items.map(function (element) {
           return <Card item={element} />
         })}
       </div>
@@ -32,4 +50,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
