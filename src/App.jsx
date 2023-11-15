9da66a58c8aa402102335d3b9bcfe225b0814f74
Jucicarla Pires <jucipires@gmail.com>
2023-11-15 01:20:40 +0000

    Criação da página <ReadById />

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home.jsx';
+import ReadById from './components/ReadById/ReadById.jsx';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<ReadById />} />
       </Routes>
     </BrowserRouter>  
     
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,20 @@
 import Tag from "../Tag/tag"
+import {Link} from "react-router-dom"
 
 export default function Card(props) {
   const item = props.item
 
   const tags = item.tags || []
 
-  return <div className="card">
-    <h2>{item.name}</h2>
-    <div className="tags">
-      {tags.map(function (element) {
-        return <Tag text={element} />
+  return <Link to={`/${item.id}`}>
+    <div className="card">
+      <h2>{item.name}</h2>
+      <div className="tags">
+        {tags.map(function (element) {
+          return <Tag text={element} />
       })}
     </div>
     <img src={item.image} />
   </div>
+  </Link>
 }
